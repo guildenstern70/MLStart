896378e062d8674ef83c195d547e8e7aaf753b8f
Alessio Saltarin <alessiosaltarin@gmail.com>
2021-08-23 16:41:58 +0200

    Added Jupyter notebook

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import mglearn
 
 
 def describe_dataset(dataset):
@@ -20,11 +22,16 @@ def main():
     describe_dataset(iris_dataset)
     X_train, X_test, y_train, y_test = train_test_split(
         iris_dataset['data'], iris_dataset['target'], random_state=0)
-    print("\nTrain data:")
-    print(" X_train shape:", X_train.shape)
-    print(" y_train shape:", y_train.shape)
+    # create dataframe from data in X_train
+    # label the columns using the strings in iris_dataset.feature_names
+    iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+    # create a scatter matrix from the dataframe, color by y_train
+    pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15),
+                               marker='o', hist_kwds={'bins': 20}, s=60,
+                               alpha=.8, cmap=mglearn.cm3)
 
 
 if __name__ == '__main__':
     print("MLStart v.0.1")
+    print("pandas version:", pd.__version__)
     main()
